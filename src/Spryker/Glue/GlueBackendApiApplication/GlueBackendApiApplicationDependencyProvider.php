8c5dca5455b6ecc9a935a31633b5e996e6668caa
Alberto Reyer <alberto.reyer@spryker.com>
2021-09-24 12:11:11 +0200

    TE-9873 Glue Backend API code infrastructure
    - move routing to JsonApi bundle, as it is coupled to the transport & format

--- src/Spryker/Glue/GlueBackendApiApplication/GlueBackendApiApplicationDependencyProvider.php
+++ src/Spryker/Glue/GlueBackendApiApplication/GlueBackendApiApplicationDependencyProvider.php
@@ -7,26 +7,37 @@
 
 namespace Spryker\Glue\GlueBackendApiApplication;
 
-use Spryker\Glue\GlueApplication\GlueApplicationDependencyProvider;
+use Spryker\Glue\EventDispatcher\Plugin\Application\EventDispatcherApplicationPlugin;
+use Spryker\Glue\GlueApplication\Plugin\Application\GlueApplicationApplicationPlugin;
+use Spryker\Glue\GlueJsonApi\Plugin\RouteRequestMatcherPlugin;
+use Spryker\Glue\GlueJsonApiExtension\Dependency\Plugin\ResourceRoutePluginInterface;
+use Spryker\Glue\HelloBackendRestApi\Plugin\GlueApplication\HelloBackendResourceRoutePlugin;
+use Spryker\Glue\Http\Plugin\Application\HttpApplicationPlugin;
 use Spryker\Glue\Kernel\AbstractBundleDependencyProvider;
 use Spryker\Glue\Kernel\Container;
+use Spryker\Glue\Router\Plugin\Application\RouterApplicationPlugin;
+use Spryker\Glue\Session\Plugin\Application\SessionApplicationPlugin;
+use Spryker\Shared\ApplicationExtension\Dependency\Plugin\ApplicationPluginInterface;
 
 class GlueBackendApiApplicationDependencyProvider extends AbstractBundleDependencyProvider
 {
-    /**
-     * @var string
-     */
+    /** @var string */
     public const PLUGIN_RESOURCE_ROUTES = 'PLUGIN_RESOURCE_ROUTES';
+    /** @var string */
+    public const PLUGIN_APPLICATIONS = 'PLUGIN_APPLICATIONS';
+    /** @var string */
+    public const PLUGIN_REQUEST_MATCHER = 'PLUGIN_REQUEST_MATCHER';
 
     /**
      * @param Container $container
-     *
      * @return Container
      */
     public function provideDependencies(Container $container)
     {
         $container = parent::provideDependencies($container);
+        $container = $this->addApplicationPlugins($container);
         $container = $this->addResourceRoutePlugins($container);
+        $container = $this->addRequestMatcherPlugin($container);
 
         return $container;
     }
@@ -48,10 +59,55 @@ class GlueBackendApiApplicationDependencyProvider extends AbstractBundleDependen
     /**
      * Rest resource route plugin stack
      *
-     * @return \Spryker\Glue\GlueApplicationExtension\Dependency\Plugin\ResourceRoutePluginInterface[]
+     * @return ResourceRoutePluginInterface
      */
     protected function getResourceRoutePlugins(): array
     {
-        return [];
+        return [
+            new HelloBackendResourceRoutePlugin(), //@todo needs to be wired on project level
+        ];
+    }
+
+    /**
+     * @param Container $container
+     *
+     * @return Container
+     */
+    protected function addApplicationPlugins(Container $container)
+    {
+        $container->set(static::PLUGIN_APPLICATIONS, function (Container $container) {
+            return $this->getApplicationPlugins();
+        });
+
+        return $container;
+    }
+
+    /**
+     * @return ApplicationPluginInterface[]
+     */
+    protected function getApplicationPlugins()
+    {
+        //@todo wiring should happen in the project. Also most plugins are probably not necessary
+        return [
+            new HttpApplicationPlugin(),
+            new SessionApplicationPlugin(),
+            new EventDispatcherApplicationPlugin(),
+            new GlueApplicationApplicationPlugin(),
+            new RouterApplicationPlugin(),
+        ];
+    }
+
+    /**
+     * @param Container $container
+     *
+     * @return Container
+     */
+    protected function addRequestMatcherPlugin(Container $container)
+    {
+        $container->set(static::PLUGIN_REQUEST_MATCHER, function () {
+            return new RouteRequestMatcherPlugin($this->getResourceRoutePlugins());
+        });
+
+        return $container;
     }
 }
--- src/Spryker/Glue/GlueBackendApiApplication/Plugin/GlueApplication/GlueBackendApiApplicationPlugin.php
+++ src/Spryker/Glue/GlueBackendApiApplication/Plugin/GlueApplication/GlueBackendApiApplicationPlugin.php
@@ -7,16 +7,16 @@
 
 namespace Spryker\Glue\GlueBackendApiApplication\Plugin\GlueApplication;
 
+use Spryker\Glue\GlueJsonApi\Plugin\AbstractGlueJsonApiApplicationPlugin;
 use Spryker\Glue\GlueApplication\ApiApplication\ApiApplicationContext;
 use Spryker\Glue\GlueApplication\Plugin\ApiApplication\HostApplicationApiContextExpander;
 use Spryker\Glue\GlueApplicationExtension\Dependency\Plugin\ApiApplicationPluginInterface;
-use Spryker\Glue\GlueApplicationExtension\Dependency\Plugin\ResourceRelationshipCollectionInterface;
-use Spryker\Glue\Kernel\AbstractPlugin;
+use Spryker\Glue\GlueJsonApi\Plugin\RouteRequestMatcherPlugin;
 
 /**
  * @method \Spryker\Glue\GlueBackendApiApplication\GlueBackendApiApplicationFactory getFactory()
  */
-class GlueBackendApiApplicationPlugin extends AbstractPlugin implements ApiApplicationPluginInterface
+class GlueBackendApiApplicationPlugin extends AbstractGlueJsonApiApplicationPlugin implements ApiApplicationPluginInterface
 {
     /**
      * @param \Spryker\Glue\GlueApplication\ApiApplication\ApiApplicationContext $apiApplicationContext
@@ -39,136 +39,10 @@ class GlueBackendApiApplicationPlugin extends AbstractPlugin implements ApiAppli
         return $this->getFactory()->getApplicationPlugins();
     }
 
-    /**
-     * Rest resource route plugin stack
-     *
-     * @return \Spryker\Glue\GlueApplicationExtension\Dependency\Plugin\ResourceRoutePluginInterface[]
-     */
-    public function getResourceRoutePlugins(): array
+    protected function getRouteRequestMatcherPlugin(): RouteRequestMatcherPlugin
     {
-        return $this->getFactory()->getResourceRoutePlugins();
+        return $this->getFactory()->getRouteRequestMatcherPlugin();
     }
 
-    /**
-     * Rest resource relation provider plugin collection, plugins must construct full resource by resource ids.
-     *
-     * @param \Spryker\Glue\GlueApplicationExtension\Dependency\Plugin\ResourceRelationshipCollectionInterface $resourceRelationshipCollection
-     *
-     * @return \Spryker\Glue\GlueApplicationExtension\Dependency\Plugin\ResourceRelationshipCollectionInterface
-     */
-    public function getResourceRelationshipPlugins(
-        ResourceRelationshipCollectionInterface $resourceRelationshipCollection
-    ): ResourceRelationshipCollectionInterface {
-        return $this->getFactory()->getResourceRelationshipPlugins($resourceRelationshipCollection);
-    }
 
-    /**
-     * Validate http request plugins
-     *
-     * @return \Spryker\Glue\GlueApplicationExtension\Dependency\Plugin\ValidateHttpRequestPluginInterface[]
-     */
-    public function getValidateHttpRequestPlugins(): array
-    {
-        return $this->getFactory()->getValidateHttpRequestPlugins();
-    }
-
-    /**
-     * Plugins that called before processing {@link \Spryker\Glue\Kernel\Controller\FormattedAbstractController}.
-     *
-     * @return \Spryker\Glue\GlueApplicationExtension\Dependency\Plugin\FormattedControllerBeforeActionPluginInterface[]
-     */
-    public function getFormattedControllerBeforeActionTerminatePlugins(): array
-    {
-        return $this->getFactory()->getFormattedControllerBeforeActionTerminatePlugins();
-    }
-
-    /**
-     * Format/Parse http request to internal rest resource request
-     *
-     * @return \Spryker\Glue\GlueApplicationExtension\Dependency\Plugin\FormatRequestPluginInterface[]
-     */
-    public function getFormatRequestPlugins(): array
-    {
-        return $this->getFactory()->getFormatRequestPlugins();
-    }
-
-    /**
-     * Format response data the data which will send with http response
-     *
-     * @return \Spryker\Glue\GlueApplicationExtension\Dependency\Plugin\FormatResponseDataPluginInterface[]
-     */
-    public function getFormatResponseDataPlugins(): array
-    {
-        return $this->getFactory()->getFormatResponseDataPlugins();
-    }
-
-    /**
-     * Format/add additional response headers
-     *
-     * @return \Spryker\Glue\GlueApplicationExtension\Dependency\Plugin\FormatResponseHeadersPluginInterface[]
-     */
-    public function getFormatResponseHeadersPlugins(): array
-    {
-        return $this->getFactory()->getFormatResponseHeadersPlugins();
-    }
-
-    /**
-     * @return \Spryker\Glue\GlueApplicationExtension\Dependency\Plugin\ValidateRestRequestPluginInterface[]
-     */
-    public function getValidateRestRequestPlugins(): array
-    {
-        return $this->getFactory()->getValidateRestRequestPlugins();
-    }
-
-    /**
-     * @return \Spryker\Glue\GlueApplicationExtension\Dependency\Plugin\RestRequestValidatorPluginInterface[]
-     */
-    public function getRestRequestValidatorPlugins(): array
-    {
-        return $this->getFactory()->getRestRequestValidatorPlugins();
-    }
-
-    /**
-     * @return \Spryker\Glue\GlueApplicationExtension\Dependency\Plugin\RestUserValidatorPluginInterface[]
-     */
-    public function getRestUserValidatorPlugins(): array
-    {
-        return $this->getFactory()->getRestUserValidatorPlugins();
-    }
-
-    /**
-     * Called before invoking controller action
-     *
-     * @return \Spryker\Glue\GlueApplicationExtension\Dependency\Plugin\ControllerBeforeActionPluginInterface[]
-     */
-    public function getControllerBeforeActionPlugins(): array
-    {
-        return $this->getFactory()->getControllerBeforeActionPlugins();
-    }
-
-    /**
-     * Called after done processing controller action
-     *
-     * @return \Spryker\Glue\GlueApplicationExtension\Dependency\Plugin\ControllerAfterActionPluginInterface[]
-     */
-    public function getControllerAfterActionPlugins(): array
-    {
-        return $this->getFactory()->getControllerAfterActionPlugins();
-    }
-
-    /**
-     * @return \Spryker\Glue\GlueApplicationExtension\Dependency\Plugin\RestUserFinderPluginInterface[]
-     */
-    public function getRestUserFinderPlugins(): array
-    {
-        return $this->getFactory()->getRestUserFinderPlugins();
-    }
-
-    /**
-     * @return \Spryker\Glue\GlueApplicationExtension\Dependency\Plugin\RouterParameterExpanderPluginInterface[]
-     */
-    public function getRouterParameterExpanderPlugins(): array
-    {
-        return $this->getFactory()->getRouterParameterExpanderPlugins();
-    }
 }
